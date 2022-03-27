0057893ff7b7e1283f092231b5a1e5c6d2f6bfda
CrzTosheba <fugu@list.ru>
2022-03-27 20:13:42 +0300

    Добавьте файлы проекта.

---
+++ Task2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+namespace WorkWithFiles;
+class Calculate
+
+{
+
+
+    static public void Main()
+    {
+
+        try
+        {
+            DirectoryInfo folder = new DirectoryInfo("E:/TestFolder"); // идем в папку, ну как идем, попутно проверяем есть ли она
+
+
+            long totalFolderSize = folderSize(folder);
+
+            Console.WriteLine("Размер, байт: " +
+                              totalFolderSize);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+
+    static long folderSize(DirectoryInfo folder)
+    {
+
+        long totalSizeOfDir = 0;
+
+
+        FileInfo[] allFiles = folder.GetFiles(); //все файлы в директории
+
+
+        foreach (FileInfo file in allFiles) // тащим размер каждого файла
+        {
+            totalSizeOfDir += file.Length;
+        }
+
+
+        DirectoryInfo[] subFolders = folder.GetDirectories(); //ищем вложенные папки
+
+
+        foreach (DirectoryInfo dir in subFolders) //тащим размер вложенных папок
+        {
+            totalSizeOfDir += folderSize(dir);
+        }
+
+
+        return totalSizeOfDir; //вертаем в зад общий размер
+    }
+}
---
+++ WorkWithFiles/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+
+
+namespace WorkWithFiles
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ChekFiles();
+
+
+
+        }
+        public static void ChekFiles()
+        {
+
+            string dirName = "E:/TestFolder";
+
+            // если папка существует
+            if (Directory.Exists(dirName))
+            {
+                Console.WriteLine($"Путь указан верно");
+                Console.WriteLine("Подкаталоги:");
+                string[] dirs = Directory.GetDirectories(dirName);
+                foreach (string s in dirs)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName);
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                using var watcher = new FileSystemWatcher("E:/TestFolder");
+
+                watcher.NotifyFilter = NotifyFilters.Attributes
+                                     | NotifyFilters.CreationTime
+                                     | NotifyFilters.DirectoryName
+                                     | NotifyFilters.FileName
+                                     | NotifyFilters.LastAccess
+                                     | NotifyFilters.LastWrite
+                                     | NotifyFilters.Security
+                                     | NotifyFilters.Size;
+
+                watcher.Changed += OnChanged;
+                watcher.Created += OnCreated;
+                watcher.Deleted += OnDeleted;
+                watcher.Renamed += OnRenamed;
+                watcher.Error += OnError;
+
+                watcher.Filter = "*.*";
+                watcher.IncludeSubdirectories = true;
+                watcher.EnableRaisingEvents = true;
+                Console.ReadLine();
+
+
+            }
+
+
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo("E:/TestFolder");
+                if ((dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length) == 0)
+                {
+                    Console.WriteLine($"Файлов нет");
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType != WatcherChangeTypes.Changed)
+            {
+                return;
+            }
+            Console.WriteLine($"Changed: {e.FullPath}");
+        }
+
+        private static void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            string value = $"Created: {e.FullPath}";
+            Console.WriteLine(value);
+        }
+
+        private static void OnDeleted(object sender, FileSystemEventArgs e) =>
+            Console.WriteLine($"Deleted: {e.FullPath}");
+
+        private static void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine($"Renamed:");
+            Console.WriteLine($"    Old: {e.OldFullPath}");
+            Console.WriteLine($"    New: {e.FullPath}");
+        }
+
+        private static void OnError(object sender, ErrorEventArgs e) =>
+            PrintException(e.GetException());
+
+        private static void PrintException(Exception? ex)
+        {
+            if (ex != null)
+            {
+                Console.WriteLine($"Message: {ex.Message}");
+                Console.WriteLine("Stacktrace:");
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine();
+                PrintException(ex.InnerException);
+            }
+        }
+
+
+    }
+
+}
+

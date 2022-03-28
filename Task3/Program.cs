548507cd2df852118edd4ea50e9944017545428e
CrzTosheba <fugu@list.ru>
2022-03-28 21:05:42 +0300

    Удпление файлов и проверка

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+using System.IO;
+using System.Collections;
+
+string dirName = "E:/TestFolder";
+
--- WorkWithFiles/Program.cs
+++ WorkWithFiles/Program.cs
@@ -21,55 +21,10 @@ namespace WorkWithFiles
         {
 
             string dirName = "E:/TestFolder";
-
-            // если папка существует
-            if (Directory.Exists(dirName))
-            {
-                Console.WriteLine($"Путь указан верно");
-                Console.WriteLine("Подкаталоги:");
-                string[] dirs = Directory.GetDirectories(dirName);
-                foreach (string s in dirs)
-                {
-                    Console.WriteLine(s);
-                }
-                Console.WriteLine();
-                Console.WriteLine("Файлы:");
-                string[] files = Directory.GetFiles(dirName);
-                foreach (string s in files)
-                {
-                    Console.WriteLine(s);
-                }
-
-                using var watcher = new FileSystemWatcher("E:/TestFolder");
-
-                watcher.NotifyFilter = NotifyFilters.Attributes
-                                     | NotifyFilters.CreationTime
-                                     | NotifyFilters.DirectoryName
-                                     | NotifyFilters.FileName
-                                     | NotifyFilters.LastAccess
-                                     | NotifyFilters.LastWrite
-                                     | NotifyFilters.Security
-                                     | NotifyFilters.Size;
-
-                watcher.Changed += OnChanged;
-                watcher.Created += OnCreated;
-                watcher.Deleted += OnDeleted;
-                watcher.Renamed += OnRenamed;
-                watcher.Error += OnError;
-
-                watcher.Filter = "*.*";
-                watcher.IncludeSubdirectories = true;
-                watcher.EnableRaisingEvents = true;
-                Console.ReadLine();
-
-
-            }
-
-
             try
             {
-                DirectoryInfo dirInfo = new DirectoryInfo("E:/TestFolder");
-                if ((dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length) == 0)
+                DirectoryInfo dirInfo = new DirectoryInfo(dirName);
+                if ((dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length) == 0) //ловим ошибки, например папка не найдена
                 {
                     Console.WriteLine($"Файлов нет");
                 }
@@ -79,49 +34,43 @@ namespace WorkWithFiles
             {
                 Console.WriteLine(e.Message);
             }
+            
 
-        }
-
-
-        private static void OnChanged(object sender, FileSystemEventArgs e)
-        {
-            if (e.ChangeType != WatcherChangeTypes.Changed)
+            // если папка существует
+            if (Directory.Exists(dirName))
             {
-                return;
-            }
-            Console.WriteLine($"Changed: {e.FullPath}");
+                Console.WriteLine($"Путь найден");
+                DirectoryInfo directory = new DirectoryInfo(dirName);
+                var allFiles = directory.EnumerateFiles();        // таки получаем инфу о файлах
+                var dirs = directory.EnumerateDirectories();      // получаем инфу о директориях
+
+                //убьем файлы
+                foreach (FileInfo file in allFiles)
+                {
+                    if (file.LastAccessTime < DateTime.Now.Subtract(TimeSpan.FromMinutes(30)))
+                    {
+                        file.Delete();
+                    }
+                        
+                }
+                //ну и другие директории
+                foreach (DirectoryInfo di in dirs)
+                {
+                    if (di.LastAccessTime < DateTime.Now.Subtract(TimeSpan.FromMinutes(30)))
+                    {
+                        di.Delete(true);
+                    }
+                        
+                }
+                Console.WriteLine("Completed..");
+                Console.ReadKey();
+                }
+            
+
         }
 
-        private static void OnCreated(object sender, FileSystemEventArgs e)
-        {
-            string value = $"Created: {e.FullPath}";
-            Console.WriteLine(value);
-        }
 
-        private static void OnDeleted(object sender, FileSystemEventArgs e) =>
-            Console.WriteLine($"Deleted: {e.FullPath}");
-
-        private static void OnRenamed(object sender, RenamedEventArgs e)
-        {
-            Console.WriteLine($"Renamed:");
-            Console.WriteLine($"    Old: {e.OldFullPath}");
-            Console.WriteLine($"    New: {e.FullPath}");
-        }
-
-        private static void OnError(object sender, ErrorEventArgs e) =>
-            PrintException(e.GetException());
-
-        private static void PrintException(Exception? ex)
-        {
-            if (ex != null)
-            {
-                Console.WriteLine($"Message: {ex.Message}");
-                Console.WriteLine("Stacktrace:");
-                Console.WriteLine(ex.StackTrace);
-                Console.WriteLine();
-                PrintException(ex.InnerException);
-            }
-        }
+        
 
 
     }
